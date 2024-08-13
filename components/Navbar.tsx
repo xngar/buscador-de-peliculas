32efe048d7bfa949fbd606ac4634ff10a418b256
xngar <mantonio.zr@gmail.com>
2024-08-13 15:48:04 -0400

    agregando dark mode

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import MenuItem from "./MenuItem";
 import { House, BadgeInfo } from "lucide-react";
 import Image from "next/image";
+import { ModeToggle } from "./ModeToggle";
 
 const Navbar = () => {
   return (
-    <header className="bg-slate-300 flex p-4 md:px-40 ">
+    <header className="bg-slate-300 dark:bg-slate-700 flex p-4 md:px-40 ">
       <div className="flex-1">
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
@@ -17,6 +18,7 @@ const Navbar = () => {
       <div className="max-w-[500px] min-w-[50px] flex   md:justify-around gap-7  ">
         <MenuItem dire="/" titulo="Home" icon={<House />} />
         <MenuItem dire="/about" titulo="About" icon={<BadgeInfo />} />
+        <ModeToggle />
       </div>
     </header>
   );
