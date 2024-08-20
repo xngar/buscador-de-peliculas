eb76f73e2a202cca70e0b659f6559fb1fc9dc978
xngar <mantonio.zr@gmail.com>
2024-08-20 09:51:35 -0400

    arreglo pagina error

--- app/error.tsx
+++ app/error.tsx
@@ -9,7 +9,7 @@ type ErrorProps = {
 const Error = ({ error, reset }: ErrorProps) => {
   return (
     <div className="w-full flex justify-center  items-center mt-10 flex-col px-3">
-      <p> Tuvimos un problema, lo resolveremos pronto..</p>
+      <p> Tuvimos un problema, lo resolveremos pronto...</p>
       <button
         className="bg-blue-800 p-2 rounded-lg text-white "
         onClick={() => reset()}
